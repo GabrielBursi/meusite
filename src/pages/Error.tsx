da168d955814fb264e935accc8c8ace68b58afa8
Gabriel Bursi <gabriellbursii@gmail.com>
2023-04-03 17:45:53 -0300

    página de erro

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,6 +1,26 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "../styles";
 
 export const Error = () => {
     return (
-        <div>Error</div>
+        <Wrapper>
+            <img src="https://raw.githubusercontent.com/thapatechnical/reactmultipage/b27bff8403d3729dcd652cff79d85c878a3f211a/public/images/error.svg" alt="error" />
+            <NavLink to="/">
+                <Button className="btn"> Go Back</Button>
+            </NavLink>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.section`
+    padding: 9rem 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .btn {
+        font-size: 1.8rem;
+        margin-top: 3rem;
+    }
+`;
