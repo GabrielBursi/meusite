3ad062650c839a2fa427a401ee7569e058b46415
Gabriel Bursi <gabriellbursii@gmail.com>
2023-04-06 14:48:31 -0300

    mudando mapa de posição

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,17 +5,6 @@ export const Contact = () => {
         <Wrapper>
             <h2 className="common-heading">Sinta-se a vontade para entrar em contato!</h2>
 
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234328.51678516713!2d-52.1110357195212!3d-23.410525275518232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ecd6ee42618c1f%3A0xc356592828e03925!2sMaring%C3%A1%20-%20Floriano%2C%20Maring%C3%A1%20-%20PR!5e0!3m2!1spt-BR!2sbr!4v1680553461940!5m2!1spt-BR!2sbr"
-                width="100%"
-                height="450"
-                style={{ border: 0 }}
-                allowFullScreen={true}
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-            >
-            </iframe>
-
             <div className="container">
                 <div className="contact-form">
                     <h2>+44 9 9881-1555</h2>
@@ -50,6 +39,17 @@ export const Contact = () => {
                     </form>
                 </div>
             </div>
+
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234328.51678516713!2d-52.1110357195212!3d-23.410525275518232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ecd6ee42618c1f%3A0xc356592828e03925!2sMaring%C3%A1%20-%20Floriano%2C%20Maring%C3%A1%20-%20PR!5e0!3m2!1spt-BR!2sbr!4v1680553461940!5m2!1spt-BR!2sbr"
+                width="100%"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen={true}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+            >
+            </iframe>
         </Wrapper>
     )
 }
@@ -62,6 +62,7 @@ const Wrapper = styled.section`
         .contact-form {
             max-width: 50rem;
             margin: auto;
+            margin-bottom: 2%;
             h2{
                 margin-bottom: 5%;
                 font-weight: 100;
